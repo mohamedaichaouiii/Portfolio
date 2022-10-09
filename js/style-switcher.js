6887a2909932d73c60298cd42efc4eb54c30a9f6
mohmaed aichaoui <mohamed.aichaoui.tic@gmail.com>
2022-10-09 13:57:34 +0100

    javascript

--- js/style-switcher.js
+++ js/style-switcher.js
@@ -1,4 +1,27 @@
 const styleSwitcherToggle= document.querySelector(".style-switcher-toggler");
 styleSwitcherToggle.addEventListener("click", () => {
-    document.querySelector(".style-switcher").classList.toggle("Open");
+    document.querySelector(".style-switcher").classList.toggle("open");
 })
+window.addEventListener("scroll", () => {
+    if( document.querySelector(".style-switcher").classList.contains("Open"))
+    {
+        document.querySelector(".style-switcher").classList.remove("Open");
+    }
+})
+const alternateStyles = document.querySelectorAll(".alternate-style");
+function setActiveStyle(color)
+ {
+    alternateStyles.forEach((style) => {
+
+        if(color === style.getAttribute("title"))
+        {
+            style.removeAttribute("disabled");
+
+        }
+        else
+        {
+            style.setAttribute("disabled","true");
+        }
+
+    })
+};
